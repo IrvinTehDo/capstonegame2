570d558f91b09eeaf0458b2732d261b78e52d47a
Irvin Do <irvindo@yahoo.com>
2020-02-11 10:14:55 -0500

    Grid now draws while mouse is held TODO: make it smoother

--- src.js
+++ src.js
@@ -2,30 +2,22 @@ const c = document.getElementById("gameCanvas");
 const ctx = c.getContext("2d");
 const CENTER_X = 100;
 const CENTER_Y = 100;
-const elements = [];
-const circleColors = ['red', 'green', 'blue', 'cyan']
+const grid = [];
+var lineSession = [];
+const circleColors = ['red', 'green', 'blue', 'cyan'];;
 var selected = null;
+var mouseDown = false;
 
 // function to get circle x,y, and radius
-function elem(x, y, radius) {
+function elem(x, y, radius, color) {
     this.x = x;
     this.y = y;
     this.radius = radius;
-}
-
-// function for clicking
-
-function isClicked (c1, c2) {
-    var a = c1.r + c2.r,
-        x = c1.x - c2.x,
-        y = c1.y - c2.y;
-
-    if ( a > Math.sqrt( (x*x) + (y*y) ) ) return true;
-    else return false;
+    this.color = color;
 }
 
 // function to draw circles
-const drawOvalShape = (ctx, center_x, center_y, radius, color) =>{
+const DrawOvalShape = (ctx, center_x, center_y, radius, color) =>{
     ctx.beginPath();
     ctx.fillStyle = color;
     ctx.arc(center_x, center_y, radius, 0, 2 * Math.PI);
@@ -42,53 +34,49 @@ const drawRect = (ctx, x, y, width, height, color) => {
     ctx.closePath();
 };
 
+const GenerateGrid = () => {
+    for(i = 1; i < 10; i++){  // 9 circles columns
+        for(j = 1; j < 7; j++){ // 7 rows
+            var color = circleColors[Math.floor(Math.random() * circleColors.length)];
+            const newEl = new elem(CENTER_X * i, CENTER_Y * j, 25, color);
+            grid.push(newEl);
+        }
+    }
+}
+
 
 // updates the line movement
 const Update = () => {
-    
 };
 
 // draw it
 
 const Draw = () => {
     ctx.clearRect(0,0,1000,1000);
-    
-    for(i = 1; i < 10; i++){  // 9 circles columns
-        for(j = 1; j < 7; j++){ // 7 rows
-            var cColor = circleColors[Math.floor(Math.random() * circleColors.length)];
-            drawOvalShape(ctx, CENTER_X * i, CENTER_Y * j, 25, cColor);
-            newel = new elem(CENTER_X * i, CENTER_Y * j, 25)
-            elements.push(newel);
-        }
+    Connect();
+    for(var i = 0; i < grid.length; i++){
+        DrawOvalShape(ctx, grid[i].x, grid[i].y, 25, grid[i].color);
     }
-    console.log(elements);  
-
-    c.addEventListener('mousedown', function (e){
-        connect(e);
-    })
 }
 
-// function to connect line
-function connect (e) {
-    var i = 0, col = null;
-    for (i = 0; i < elements.length; i++) {
-        var element = elements[i],
-            c1 = {x: element.x, y: element.y, r: 25},
-            c2 = {x: e.clientX, y: e.clientY, r: 25};
-        if (isClicked(c1, c2)) col = element;
+const Connect = () => {
+    if(lineSession.length <= 1){
+        return;
     }
 
-    if (col !== null) {
-        if(selected !== null) drawLine(col);
-        selected = col;
+    for (i = 1; i < grid.length; i++){
+        const previous = lineSession[i - 1];
+        const current = lineSession[i];
+        drawLine(previous, current);
     }
-    else selected = null;
 }
 
 
-function drawLine (element) {
+function drawLine (previous, element) {
+    if(!element || !previous) return;
+
     ctx.beginPath();
-    ctx.moveTo(selected.x, selected.y);
+    ctx.moveTo(previous.x, previous.y);
     ctx.lineTo(element.x, element.y);
     ctx.lineWidth = 5;
     ctx.strokeStyle = "black";
@@ -105,12 +93,45 @@ window.addEventListener("keypress", (e) => {
 
 });
 
-Draw();
+c.addEventListener('mousedown', function (e){
+    mouseDown = true;
+    Connect(e);
+});
+
+c.addEventListener('mouseup', function (e){
+    mouseDown = false;
+    lineSession = [];
+    CheckScore();
+});
+
+const MouseInCircle = (circle, mouse) => {
+    const distX = mouse.x - circle.x;
+    const distY = mouse.y - circle.y;
+    const dist = Math.sqrt((distX*distX) + (distY*distY));
+    if(dist <= circle.radius){
+        return true;
+    }
+    return false;
+};
+
+c.addEventListener('mousemove', function (e){
+    if(mouseDown){
+        const mouse = {x: e.clientX, y: e.clientY};
+        for(let i = 0; i < grid.length; i++){
+            if(MouseInCircle(grid[i], mouse)){
+                lineSession.push(grid[i]);
+            } else {
+
+            };
+        }
+    }
+});
+
+GenerateGrid();
 
 // Draw at 60fps
-// setInterval(() => {
-// //    Update();
-//     
-    
-// });
+setInterval(() => {
+   Update();
+   Draw();
+}, 1000/60);
 
